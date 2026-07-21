357d64294b6de9a03536a7ded74dedb578654ce7
JoleneZou <jolene.zou@holidayextras.com>
2026-07-21 13:26:14 +0100

    Link download buttons to Snidge 1.2.0

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 // 下载链接：文件名以后可能调整，改这里就行
 const DOWNLOAD_URLS = {
-  windows: 'https://github.com/RaidenVal/snidge/releases/latest/download/snidge-setup.exe',
-  mac: 'https://github.com/RaidenVal/snidge/releases/latest/download/snidge.dmg',
+  windows:
+    'https://github.com/RaidenVal/snidge/releases/latest/download/snidge-1.2.0-setup.exe',
+  mac: 'https://github.com/RaidenVal/snidge/releases/latest/download/snidge-1.2.0.dmg',
 };
 
 function setupDownloads() {
